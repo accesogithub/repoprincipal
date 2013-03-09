401be20bbbdfd2f352066278d32c18fa65ef605f
junior <junior@hotmail.com>
2013-03-09 01:52:09 -0500

    Mi segundo commit

---
+++ Factorial.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+	printf("Prueba de un archivo en c");
+}
---
+++ Persona.java
@@ -0,0 +1,6 @@
+public class Persona{
+	private int Id;
+	private String nombres;
+	private String apellidos;
+	
+}
